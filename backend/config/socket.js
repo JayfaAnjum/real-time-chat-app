507feda8a30ad1e111d189d88bd2f13daca52885
JayfaAnjum <jayfatha62@gmail.com>
2026-01-08 21:51:46 +0530

    done

--- backend/config/socket.js
+++ backend/config/socket.js
@@ -1,7 +1,6 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
-import jwt from "jsonwebtoken";
 
 const app = express();
 const server = http.createServer(app);
@@ -9,36 +8,28 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: ["https://real-time-chat-app-one-sable.vercel.app"],
-    methods: ["GET", "POST"],
-    credentials: true, // needed if you also use cookies
   },
 });
 
+export function getReceiverSocketId(userId) {
+  return userSocketMap[userId];
+}
+
+// used to store online users
 const userSocketMap = {}; // {userId: socketId}
 
-io.use((socket, next) => {
-  const token = socket.handshake.auth.token;
-  if (!token) return next(new Error("No token provided"));
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET); // your secret
-    socket.userId = decoded.id; // attach userId to socket
-    next();
-  } catch (err) {
-    next(new Error("Invalid token"));
-  }
-});
-
 io.on("connection", (socket) => {
-  console.log("A user connected", socket.userId, socket.id);
-  userSocketMap[socket.userId] = socket.id;
+  console.log("A user connected", socket.id);
 
-  // Notify all clients about online users
+  const userId = socket.handshake.auth.userId;
+  if (userId) userSocketMap[userId] = socket.id;
+
+  // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
-    console.log("A user disconnected", socket.userId);
-    delete userSocketMap[socket.userId];
+    console.log("A user disconnected", socket.id);
+    delete userSocketMap[userId];
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
 });
